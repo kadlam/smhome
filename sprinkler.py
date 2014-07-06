a58bdd62964512c85e1f4bdf6ed016cd5a048833
Eric Adlam <eadlam@gmail.com>
2014-07-06 16:37:17 -0600

    moved GPIO setup to bot init

--- sprinkler.py
+++ sprinkler.py
@@ -1,20 +1,19 @@
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BOARD)
-
-GPIO.setup(11, GPIO.OUT)
-GPIO.setup(13, GPIO.OUT)
-
-GPIO.cleanup()
-
 class Bot():
 
 	def __init__(self):
 		self.GPIO = GPIO
+		self.GPIO.setmode(self.GPIO.BOARD)
+
+		self.GPIO.setup(11, self.GPIO.OUT)
+		self.GPIO.setup(13, self.GPIO.OUT)
 
 	def turnOn(self, n):
-		self.GPIO.cleanup()
 		self.GPIO.output(n, 1)
+
+	def turnOff(self, n):
+		self.GPIO.output(n, 0)
 
 	def off(self):
 		self.GPIO.cleanup()
