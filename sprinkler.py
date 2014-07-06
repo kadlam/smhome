6383582d269197bb5f7afd4252bd74c2719d0e95
Eric Adlam <eadlam@gmail.com>
2014-07-06 16:41:47 -0600

    moved cleanup so that it doesnt override value setting

--- sprinkler.py
+++ sprinkler.py
@@ -5,16 +5,12 @@ class Bot():
 	def __init__(self):
 		self.GPIO = GPIO
 		self.GPIO.setmode(self.GPIO.BOARD)
-
-		self.GPIO.setup(11, self.GPIO.OUT)
-		self.GPIO.setup(13, self.GPIO.OUT)
+		self.GPIO.cleanup()
 
 	def turnOn(self, n):
 		self.GPIO.cleanup()
+		self.GPIO.setup(n, self.GPIO.OUT)
 		self.GPIO.output(n, 1)
 
-	def turnOff(self, n):
-		self.GPIO.output(n, 0)
-
-	def off(self):
+	def cleanup(self):
 		self.GPIO.cleanup()
