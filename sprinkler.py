f7036aa0580d54a0d0b11b574fae58728c53323c
Eric Adlam <eadlam@gmail.com>
2014-07-06 16:46:30 -0600

    moved GPIO out of scope of Bot

--- sprinkler.py
+++ sprinkler.py
@@ -3,14 +3,12 @@ import RPi.GPIO as GPIO
 class Bot():
 
 	def __init__(self):
-		self.GPIO = GPIO
-		self.GPIO.setmode(self.GPIO.BOARD)
-		self.GPIO.cleanup()
+		GPIO.setmode(GPIO.BOARD)
 
 	def turnOn(self, n):
-		self.GPIO.cleanup()
-		self.GPIO.setup(n, self.GPIO.OUT)
-		self.GPIO.output(n, 1)
+		GPIO.cleanup()
+		GPIO.setup(n, GPIO.OUT)
+		GPIO.output(n, 1)
 
-	def cleanup(self):
-		self.GPIO.cleanup()
+	def turnOff(self):
+		GPIO.cleanup()
