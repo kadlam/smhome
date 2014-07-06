68ec16d5f3a973929c5f23bab2c9e0014906e41a
Eric Adlam <eadlam@gmail.com>
2014-07-06 16:52:58 -0600

    initializing all switches

--- sprinkler.py
+++ sprinkler.py
@@ -2,8 +2,13 @@ import RPi.GPIO as GPIO
 
 class Bot():
 
+	def all(self):
+		return [11,13,15]
+
 	def __init__(self):
 		GPIO.setmode(GPIO.BOARD)
+		for switch in self.all():
+			GPIO.setup(switch, GPIO.OUT)
 
 	def turnOn(self, n):
 		GPIO.cleanup()
