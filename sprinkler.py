6392954dafa94005ca4beeae0b8cdef28940582a
kadlam <linuxone11@gmail.com>
2014-07-25 21:33:07 -0600

    add sprinkler.py

--- sprinkler.py
+++ sprinkler.py
@@ -3,7 +3,7 @@ import RPi.GPIO as GPIO
 class Bot():
 
 	def all(self):
-		return [11,13,15]
+		return [11,13,15,19,21,23]
 
 	def __init__(self):
 		GPIO.setmode(GPIO.BOARD)
@@ -20,5 +20,4 @@ class Bot():
 
 	def cleanup(self):
 		GPIO.cleanup()
-
 	
