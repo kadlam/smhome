0448cacaf95f543d441ae2ee72d9a06fa5214efe
kadlam <linuxone11@gmail.com>
2014-07-27 17:33:28 -0600

    pushing sprinklerservice & contents

---
+++ sprinklerservice/switches/migrations/0001_initial.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Switch',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False)),
+                ('name', models.CharField(max_length=200)),
+                ('location', models.CharField(max_length=200)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
