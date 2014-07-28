525cf1e5ccabbbe25140cf02425d5572fe4f1242
kadlam <linuxone11@gmail.com>
2014-07-27 20:05:52 -0600

    more misc. cleanup

--- switches/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from switches.models import Switch
-
-admin.site.register(Switch)
--- switches/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Switch(models.Model):
-	name = models.CharField(max_length=200)
-	location = models.CharField(max_length=200)
-
-	def __str__(self):
-		return self.name + ', ' + self.location
