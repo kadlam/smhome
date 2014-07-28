a1af7d7025aefa4104cd612ad556d41c7149df5b
kadlam <linuxone11@gmail.com>
2014-07-27 20:08:04 -0600

    more misc. cleanup

---
+++ sprinklerbot/sprinkler.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+
+class Bot():
+
+	def all(self):
+		return [11,13,15,19,21,23]
+
+	def __init__(self):
+		GPIO.setmode(GPIO.BOARD)
+		for switch in self.all():
+			GPIO.setup(switch, GPIO.OUT)
+
+	def turnOff(self):
+		for switch in self.all():
+			GPIO.output(switch, 0)
+
+	def turnOn(self, n):
+		self.turnOff()
+		GPIO.output(n, 1)
+
+	def cleanup(self):
+		GPIO.cleanup()
+	
+#created by eadlam June/July 2014
+#copied to github kadlam 25-JUL-2014
